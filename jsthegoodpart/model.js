8fcd2ff644470e3f12f3b1ec0e456ef650087ee3
TinaC <skyjunechentian@sina.com>
2016-03-21 17:30:52 +0800

    functional

--- jsthegoodpart/model.js
+++ jsthegoodpart/model.js
@@ -52,3 +52,30 @@ var seqer = serial_marker();
 seqer.set_prefix('Q');
 seqer.set_seq(1000);
 var unique = seqer.gensym();
+
+// 由于语言的一个设计错误，js函数中的arguments并不是真正的数组，它只是一个“类似数组”的对象，拥有一个length属性，但它没有任何数组的方法。所以需要两个arguments数组上都应用数组的slice方法
+Function.method('curry', function () {
+	var slice = Array.prototype.slice,
+		args = slice.apply(arguments),
+		that = this;
+	return function () {
+		return that.apply(null, args.concat(slice.apply(arguments)));
+	}
+})
+
+// momemoizer
+var fibonacci = function () {
+	var memo = [0,1];
+	var fib = function (n) {
+		var result = memo[n];
+		if (typeof result !== 'number') {
+			result = fib(n-1) + fib(n-2);
+			memo[n] = result;
+		}
+		return result;
+	}
+}
+
+for(var i = 0; i <= 10; i++){
+	document.writeln("//" + i + ":" + fibonacci(i));
+}
